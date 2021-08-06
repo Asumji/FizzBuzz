951f78be280c77bf9c14628616a458486e6983bd
Asumji <64544378+Asumji@users.noreply.github.com>
2021-08-06 12:48:03 +0200

    how did I not know about %

--- index.py
+++ index.py
@@ -1,23 +1,16 @@
 import time
-by5 = 0
-by3 = 0
+
 count = 0
-while True:
+
+for i in range(100):
     time.sleep(0.3)
     count += 1
-    by5 = count / 5
-    by3 = count / 3
-    by5 = str(by5)
-    by3 = str(by3)
     
-    by5array = by5.split(".")
-    by3array = by3.split(".")
-    
-    if (by3array[1][0] == "0" and by5array[1][0] == "0"):
+    if (count % 5 == 0 and count % 3 == 0):
         print("FizzBuzz")
-    elif (by3array[1][0] == "0"):
+    elif (count % 3 == 0):
         print("Fizz")
-    elif (by5array[1][0] == "0"):
+    elif (count % 5 == 0):
         print("Buzz")
     else:
         print(count)
